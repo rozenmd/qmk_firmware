a33b8681ad3157fa6295eaa32bb323f0da9773b3
Ibnu D. Aji <iaji@siskam.link>
2018-10-27 12:38:13 +0700

    updated readme.md.

--- keyboards/atreus/keymaps/cacat/keymap.c
+++ keyboards/atreus/keymaps/cacat/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                      KC_Y,   KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                      KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,                      KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, MO(_QS), KC_LCTL, KC_SPC,  KC_LALT, KC_MINS, KC_SLSH, KC_ENT ),
+    KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, MO(_QS), KC_LCTL, KC_SPC, KC_LALT, KC_MINS, KC_SLSH, KC_ENT ),
 
   [_QS] = LAYOUT(
     KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_BSLS, KC_7,    KC_8,   KC_9,   KC_ASTR ,
