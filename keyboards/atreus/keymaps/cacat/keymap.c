238d6bb776001dc2dabf6195e625aea51538afb1
Ibnu D. Aji <iaji@siskam.link>
2018-10-28 23:48:25 +0700

    added mod taps.

--- keyboards/atreus/keymaps/cacat/keymap.c
+++ keyboards/atreus/keymaps/cacat/keymap.c
@@ -14,12 +14,16 @@
 #define _QR 4
 #define _QL 5
 
+#define CT_QUOT CTL_T(KC_QUOT)
+#define CT_Z    CTL_T(KC_Z)
+#define SH_ESC  SFT_T(KC_ESC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DV] = LAYOUT(
     KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L ,
     KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S ,
-    KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,                      KC_B,    KC_M,    KC_W,    KC_V,    KC_Z ,
-    KC_ESC,  KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC, MO(_RS), KC_LCTL, KC_SPC,  KC_LALT, KC_MINS, KC_SLSH, KC_ENT
+    CT_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,                      KC_B,    KC_M,    KC_W,    KC_V,    KC_Z ,
+    KC_ESC,  KC_TAB,  KC_LGUI, SH_ESC,  KC_BSPC, MO(_RS), KC_LCTL, KC_SPC,  KC_LALT, KC_MINS, KC_SLSH, KC_ENT
   ),
 
   /*
@@ -48,8 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT(
     KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                      KC_Y,   KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                      KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN ,
-    KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,                      KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, MO(_QR), KC_LCTL, KC_SPC, KC_LALT, KC_MINS, KC_SLSH, KC_ENT ),
+    CT_Z,   KC_X,   KC_C,    KC_V,    KC_B,                      KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
+    KC_ESC, KC_TAB, KC_LGUI, SH_ESC,  KC_BSPC, MO(_QR), KC_LCTL, KC_SPC, KC_LALT, KC_MINS, KC_SLSH, KC_ENT ),
 
   [_QR] = LAYOUT(
     KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_BSLS, KC_7,    KC_8,   KC_9,   KC_ASTR ,
