b409b8d5e4c39e8ca13c9968320fc491b48b2eb0
Ibnu D. Aji <iaji@siskam.link>
2018-10-30 21:54:20 +0700

    changed something. my thumb hurts.

--- keyboards/atreus/keymaps/cacat/keymap.c
+++ keyboards/atreus/keymaps/cacat/keymap.c
@@ -27,8 +27,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DV] = LAYOUT(
     KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L ,
     KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S ,
-    KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,                      KC_B,    KC_M,    KC_W,    KC_V,    KC_Z ,
-    CT_ESC,  KC_TAB,  KC_LGUI, SH_ENT,  KC_BSPC, MO(_RS), KC_LCTL, KC_SPC,  ALT_ESC, KC_MINS, KC_SLSH, KC_ENT
+    CT_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,                      KC_B,    KC_M,    KC_W,    KC_V,    KC_Z ,
+    KC_ESC,  KC_TAB,  KC_LGUI, SH_ENT,  KC_BSPC, MO(_RS), KC_LCTL, KC_SPC,  ALT_ESC, KC_MINS, KC_SLSH, KC_ENT
   ),
 
   /*
