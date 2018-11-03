33414f3cee40a81c49e763315556fa28288e8fcb
Ibnu D. Aji <iaji@siskam.link>
2018-11-04 02:23:10 +0700

    changed something. my thumb hurts.

--- keyboards/atreus/keymaps/cacat/keymap.c
+++ keyboards/atreus/keymaps/cacat/keymap.c
@@ -8,37 +8,47 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _DV 0
-#define _DR 1
-#define _DL 2
-#define _QW 3
-#define _QR 4
-#define _QL 5
+#define _DL 1
+#define _DR 2
+#define _DA 3
+#define _QW 4
+#define _QR 5
+#define _QL 6
+
+#define _____   KC_TRNS
 
 #define CT_QUOT CTL_T(KC_QUOT)
 #define CT_Z    CTL_T(KC_Z)
 #define SH_ESC  SFT_T(KC_ESC)
 #define ALT_ENT ALT_T(KC_ENT)
 
-#define CT_ESC  CTL_T(KC_ESC)
-#define SH_ENT  SFT_T(KC_ENT)
-#define SH_Z    SFT_T(KC_Z)
+#define ALBSPC  ALT_T(KC_BSPC)
 #define ALT_ESC ALT_T(KC_ESC)
+#define CT_ESC  CTL_T(KC_ESC)
+#define DL_BSPC LT(_DL, KC_BSPC)
 #define DR_SPC  LT(_DR, KC_SPC)
 #define QR_SPC  LT(_QR, KC_SPC)
-#define ALBSPC  ALT_T(KC_BSPC)
+#define SH_ENT  SFT_T(KC_ENT)
+#define SH_Z    SFT_T(KC_Z)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*
-   *  ;        ,        .         p        y            ||          f        g       c         r        l
-   *  a        o        e         u        i            ||          d        h       t         n        s
-   *  '        q        j         k        j            ||          b        m       w         v        z
-   * Esc      Tab      Win     SH/Ent   ALT/Bsp RAISE   || Ctrl   RAISE/Spc  ALT/Esc   -         /       Ent
+   *  ;        ,        .         p        y             ||          f        g       c         r        l
+   *  a        o        e         u        i             ||          d        h       t         n        s
+   *  '        q        j         k        j             ||          b        m       w         v        z
+   * Esc      Tab      Win     SH/Ent   ALT/Bsp  RAISE   || Ctrl   RAISE/Spc  ALT/Esc   -         /       Ent
    */
   [_DV] = LAYOUT(
-    KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L   ,
-    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S   ,
-    CT_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,                      KC_B,    KC_M,    KC_W,    KC_V,    SH_Z   ,
-    KC_ESC,  KC_TAB,  KC_LGUI, SH_ENT,  ALBSPC, MO(_DR), KC_LCTL,  DR_SPC,  ALT_ESC, KC_MINS, KC_SLSH, KC_ENT ),
+    KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,                       KC_F,    KC_G,    KC_C,    KC_R,    KC_L   ,
+    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                       KC_D,    KC_H,    KC_T,    KC_N,    KC_S   ,
+    CT_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,                       KC_B,    KC_M,    KC_W,    KC_V,    SH_Z   ,
+    KC_ESC,  KC_TAB,  KC_LGUI, SH_ENT,  DL_BSPC, MO(_DR), KC_LCTL,  DR_SPC,  ALT_ESC, KC_MINS, KC_SLSH, KC_ENT ),
+
+  [_DL] = LAYOUT( /* [> LOWER <] */
+    KC_ESC,  _____, _____, _____, _____,                   _____, _____, _____, _____,   _____   ,
+    KC_TAB,  _____, _____, _____, _____,                   _____, _____, _____, _____,   KC_ENT  ,
+    KC_LSFT, _____, _____, _____, _____,                   _____, _____, _____, KC_MINS, KC_SLSH ,
+    _____,   _____, _____, _____, _____, KC_LALT, KC_LCTL, _____, _____, _____, _____,   _____   ),
 
   /*
    *  !        @       Up        {        }             ||           \        7       8       9         *
@@ -50,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_BSLS, KC_7,    KC_8,   KC_9,   KC_ASTR ,
     KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                    KC_EQL,  KC_4,    KC_5,   KC_6,   KC_PLUS ,
     KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                   KC_GRV,  KC_1,    KC_2,   KC_3,   KC_PGUP ,
-    TG(_DL), KC_INS,  KC_LGUI, KC_LSFT, KC_DELT, KC_TRNS, KC_LCTL, KC_SPC,  ALT_ENT, KC_0,   KC_DOT, KC_PGDN ),
+    TG(_DA), KC_INS,  KC_LGUI, KC_LSFT, KC_DELT, KC_TRNS, KC_LCTL, KC_SPC,  ALT_ENT, KC_0,   KC_DOT, KC_PGDN ),
 
   /*
    *  Ins    Home       Up       End      PgUp          ||          Up       F7       F8       F9      F10
@@ -58,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *         VolUp                        RESET         ||        QWERTY     F1       F2       F3      F12
    *         VolDn      Win      Shift    Bspc   DVORAK || Ctrl    space   ALT/Ent   PrtSc   ScrLk    Pause
    */
-  [_DL] = LAYOUT( /* [> LOWER <] */
+  [_DA] = LAYOUT( /* [> ADJUST <] */
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
     KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
     KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,                     TO(_QW), KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
