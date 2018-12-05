9330695a552ee75cf319210564a146ef68b45615
Ibnu D. Aji <iaji@siskam.link>
2018-12-05 14:14:44 +0700

    prepare something.

---
+++ keyboards/handwired/daktil_mini/daktil_mini.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "4x5.h"
+
+//void promicro_bootloader_jmp(bool program);
+#include "quantum.h"
+
+
+#ifdef USE_I2C
+#include <stddef.h>
+#ifdef __AVR__
+	#include <avr/io.h>
+	#include <avr/interrupt.h>
+#endif
+#endif
+
+
