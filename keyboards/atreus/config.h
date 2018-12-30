7ca91e0901678e282c09313c446b22ed4e840d35
Ibnu D. Aji <iaji@siskam.link>
2018-12-30 18:48:21 +0700

    salah solder

--- keyboards/atreus/config.h
+++ keyboards/atreus/config.h
@@ -38,7 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Change this to how you wired your keyboard
 // COLS: Left to right, ROWS: Top to bottom
 #define MATRIX_ROW_PINS { D3, F4, F5, D1 }
-#define MATRIX_COL_PINS { F6, F7, B1, B2, B3, B6, B5, B4, E6, D7, C6 }
+#define MATRIX_COL_PINS { F6, B1, F7, B2, B3, B6, B5, B4, E6, D7, C6 }
 
   /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
