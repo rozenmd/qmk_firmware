956c6264fbd63e1afb8196835c7ad32a94d0ea31
Ibnu D. Aji <iaji@siskam.link>
2019-01-02 01:19:04 +0700

    forgot some things.

--- keyboards/atreus/keymaps/cacat/keymap.c
+++ keyboards/atreus/keymaps/cacat/keymap.c
@@ -44,13 +44,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,                     KC_F,    KC_G,    KC_C,    KC_R,    KC_L   ,
     KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                     KC_D,    KC_H,    KC_T,    KC_N,    KC_S   ,
     KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,                     KC_B,    KC_M,    KC_W,    KC_V,    KC_Z   ,
-    KC_ESC,  KC_TAB,  KC_LGUI, SH_ENT,  DL_BSPC, MO(_DR), CT_ESC, DR_SPC,  ALT_ESC, KC_MINS, KC_SLSH, KC_ENT ),
+    KC_ESC,  KC_TAB,  KC_LGUI, SH_ENT,  DL_BSPC, CT_ESC, MO(_DR), DR_SPC,  ALT_ESC, KC_MINS, KC_SLSH, KC_ENT ),
 
   [_DL] = LAYOUT( /* [> LOWER <] */
     KC_ESC,  KC_PSCR, KC_PAUS, KC_F1, KC_F2,                    KC_F3,  KC_F4,  KC_INS,  KC_DELT, KC_BSPC ,
     KC_CLCK, KC_TAB,  KC_PGUP, KC_F5, KC_F6,                    KC_F7,  KC_F8,  KC_HOME, KC_LALT, KC_ENT  ,
     KC_LSFT, KC_SLCK, KC_PGDN, KC_F9, KC_F10,                   KC_F11, KC_F12, KC_END,  KC_MINS, KC_SLSH ,
-    _____,   _____,   _____,   _____, _____,  KC_LALT, KC_LCTL, _____,  _____,  KC_APP,  _____,   _____   ),
+    _____,   _____,   _____,   _____, _____,  KC_LCTL, KC_LALT, _____,  _____,  KC_APP,  _____,   _____   ),
 
   /*
    *  !        @       Up        {        }             ||           \        7       8       9         *
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_BSLS, KC_7,    KC_8,   KC_9,   KC_ASTR ,
     KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                    KC_EQL,  KC_4,    KC_5,   KC_6,   KC_PLUS ,
     KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                   KC_GRV,  KC_1,    KC_2,   KC_3,   KC_PGUP ,
-    TG(_DA), KC_INS,  KC_LGUI, KC_LSFT, KC_DELT, KC_TRNS, KC_LCTL, KC_SPC,  ALT_ENT, KC_0,   KC_DOT, KC_PGDN ),
+    TG(_DA), KC_INS,  KC_LGUI, KC_LSFT, KC_DELT, KC_LCTL, KC_TRNS, KC_SPC,  ALT_ENT, KC_0,   KC_DOT, KC_PGDN ),
 
   /*
    *  Ins    Home       Up       End      PgUp          ||          Up       F7       F8       F9      F10
@@ -74,19 +74,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
     KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
     KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,                     TO(_QW), KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, TO(_DV), KC_LCTL, KC_SPC,  ALT_ENT, KC_PSCR, KC_SLCK, KC_PAUS ),
+    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, TO(_DV), KC_SPC,  ALT_ENT, KC_PSCR, KC_SLCK, KC_PAUS ),
 
   [_QW] = LAYOUT(
-    KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
-    KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
-    CT_Z,   KC_X,   KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  SH_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI, SH_ENT,  ALBSPC, MO(_QR), KC_LCTL,  QR_SPC,  ALT_ESC, KC_MINS, KC_SLSH, KC_ENT ),
+    KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
+    KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
+    CT_Z,   KC_X,   KC_C,    KC_V,    KC_B,                     KC_N,    KC_M,    KC_COMM, KC_DOT,  SH_SLSH ,
+    KC_ESC, KC_TAB, KC_LGUI, SH_ENT,  ALBSPC, KC_LCTL, MO(_QR), QR_SPC,  ALT_ESC, KC_MINS, KC_SLSH, KC_ENT ),
 
   [_QR] = LAYOUT(
     KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_BSLS, KC_7,    KC_8,   KC_9,   KC_ASTR ,
     KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                    KC_EQL,  KC_4,    KC_5,   KC_6,   KC_PLUS ,
     KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                   KC_GRV,  KC_1,    KC_2,   KC_3,   KC_PGUP ,
-    TO(_QL), KC_INS,  KC_LGUI, KC_LSFT, KC_DELT, KC_TRNS, KC_LCTL, KC_SPC,  ALT_ENT, KC_0,   KC_DOT, KC_PGDN ),
+    TO(_QL), KC_INS,  KC_LGUI, KC_LSFT, KC_DELT, KC_LCTL, KC_TRNS, KC_SPC,  ALT_ENT, KC_0,   KC_DOT, KC_PGDN ),
 
   [_QL] = LAYOUT(
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
