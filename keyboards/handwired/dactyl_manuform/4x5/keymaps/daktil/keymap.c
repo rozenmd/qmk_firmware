060d6206627c85566d4dec163fb4e0369b8915c5
Ibnu D. Aji <iaji@siskam.link>
2019-01-25 22:58:50 +0700

    something related to wiring.

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/daktil/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/daktil/keymap.c
@@ -19,13 +19,6 @@ extern keymap_config_t keymap_config;
 #define ALT_SPC  ALT_T(KC_SPC)
 #define SFT_ENT  SFT_T(KC_ENT)
 
-#define KC_ML KC_MS_LEFT
-#define KC_MR KC_MS_RIGHT
-#define KC_MU KC_MS_UP
-#define KC_MD KC_MS_DOWN
-#define KC_MB1 KC_MS_BTN1
-#define KC_MB2 KC_MS_BTN1
-
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 #define ADJUST TG(_ADJUST)
@@ -39,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_QUOT, KC_Q,    KC_J,    KC_K, KC_X,                                     KC_B, KC_M, KC_W,    KC_V,   KC_Z,
            KC_TAB,  KC_LGUI,                                                             KC_MINS, KC_SLSH,
                                          KC_BSPC, KC_LSFT,  RAISE,   KC_SPC,
-                                         KC_LALT, LOWER,    KC_ENT,  KC_LCTL,
+                                         KC_LCTL, LOWER,    KC_ENT,  KC_LCTL,
                                          KC_DEL,  KC_ESC,   KC_LSFT, KC_LALT
 ),
 
@@ -62,22 +55,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                              ____, ____,  ____, ____
 ),
 [_ADJUST] = LAYOUT(
-    KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                          KC_BSLS, KC_7, KC_8, KC_9,   KC_ASTR ,
-    KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                           KC_EQL,  KC_4, KC_5, KC_6,   KC_PLUS ,
-    ____,    ____,    ____,    ____,    RESET,                            QWERTY,  KC_1, KC_2, KC_3,   ____ ,
-             ____,    ____,                                                              KC_0, KC_DOT,
-                                               ____, ____,  REBASE, ____,
-                                               ____, ____,  ____,   ____,
-                                               ____, ____,  ____,   ____
+    ____, ____, ____, ____, ____,                            ____,   ____, ____, ____,   ____,
+    ____, ____, ____, ____, ____,                            ____,   ____, ____, ____,   ____,
+    ____, ____, ____, ____, RESET,                           QWERTY, ____, ____, ____,   ____,
+          ____, ____,                                                      ____, ____,
+                                  ____, ____,  REBASE, ____,
+                                  ____, ____,  ____,   ____,
+                                  ____, ____,  ____,   ____
 ),
 [_QWERTY] = LAYOUT(
-    KC_Q, KC_W, KC_E, KC_R, KC_T,                                  KC_Y, KC_U, KC_I,    KC_O,   KC_P    ,
-    KC_A, KC_S, KC_D, KC_F, KC_G,                                  KC_H, KC_J, KC_K,    KC_L,   KC_SCLN ,
-    KC_Z, KC_X, KC_C, KC_V, KC_B,                                  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH ,
-          ____, ____,                                                          ____,    ____,
-                                KC_BSPC, KC_LSFT,  RAISE,   KC_SPC,
-                                KC_LALT, LOWER,    KC_ENT,  KC_LCTL,
-                                KC_DEL,  KC_ESC,   KC_LSFT, KC_LALT
+    KC_Q, KC_W, KC_E, KC_R, KC_T,                                      KC_Y, KC_U, KC_I,    KC_O,   KC_P    ,
+    KC_A, KC_S, KC_D, KC_F, KC_G,                                      KC_H, KC_J, KC_K,    KC_L,   KC_SCLN ,
+    KC_Z, KC_X, KC_C, KC_V, KC_B,                                      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH ,
+          ____, ____,                                                              KC_MINS, KC_QUOT,
+                                  KC_BSPC, KC_LSFT,  RAISE,   KC_SPC,
+                                  KC_LALT, LOWER,    KC_ENT,  KC_LCTL,
+                                  KC_DEL,  KC_ESC,   KC_LSFT, KC_LALT
 ),
 };
 
