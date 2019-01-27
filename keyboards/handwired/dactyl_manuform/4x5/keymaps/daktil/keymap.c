10095793da598a874cbdc60d0a01756d6e23c5f8
Ibnu D. Aji <iaji@siskam.link>
2019-01-27 18:47:48 +0700

    something related to wiring.

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/daktil/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/daktil/keymap.c
@@ -37,13 +37,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_RAISE] = LAYOUT(
-    KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                          KC_BSLS, KC_7, KC_8,   KC_9, KC_ASTR ,
-    KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                           KC_EQL,  KC_4, KC_5,   KC_6, KC_PLUS ,
-    KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                          KC_GRV,  KC_1, KC_2,   KC_3, KC_PGUP ,
-             KC_INS,  KC_LGUI,                                                     KC_0, KC_DOT,
-                                               ____, ____,    ____, ____,
-                                               ____, ____,    ____, ____,
-                                               ____, ADJUST,  ____, ____
+    KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                             KC_BSLS, KC_7, KC_8,   KC_9, KC_ASTR ,
+    KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                              KC_EQL,  KC_4, KC_5,   KC_6, KC_PLUS ,
+    KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                             KC_GRV,  KC_1, KC_2,   KC_3, KC_PGUP ,
+             KC_INS,  KC_LGUI,                                                        KC_0, KC_DOT,
+                                               KC_DELT, ____,    ____, ____,
+                                               ____,    ____,    ____, ____,
+                                               ____,    ADJUST,  ____, ____
 ),
 [_LOWER] = LAYOUT(
     KC_ESC,  KC_PSCR, KC_PAUS, KC_F1, KC_F2,                           KC_F3,  KC_F4,  KC_INS,  KC_DELT, KC_BSPC ,
