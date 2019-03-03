315cb813bebe86d6be5eb5b99ff5e9242dfc9c73
Ibnu D. Aji <iaji@siskam.link>
2019-03-03 19:53:52 +0700

    asoetuh.

--- keyboards/atreus/keymaps/cacat/keymap.c
+++ keyboards/atreus/keymaps/cacat/keymap.c
@@ -41,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * Esc      Tab      Win     SH/Ent   ALT/Bsp  RAISE   || Ctrl   RAISE/Spc  ALT/Esc   -         /       Ent
    */
   [_DV] = LAYOUT(
-    KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,                     KC_F,    KC_G,    KC_C,    KC_R,    KC_L   ,
-    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                     KC_D,    KC_H,    KC_T,    KC_N,    KC_S   ,
-    KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,                     KC_B,    KC_M,    KC_W,    KC_V,    KC_Z   ,
-    KC_ESC,  KC_TAB,  KC_LGUI, SH_ENT,  DL_BSPC, CT_ESC, MO(_DR), DR_SPC,  ALT_ESC, KC_MINS, KC_SLSH, KC_ENT ),
+    KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L   ,
+    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S   ,
+    KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,                      KC_B,    KC_M,    KC_W,    KC_V,    KC_Z   ,
+    KC_ESC,  KC_TAB,  KC_LGUI, SH_ENT,  DL_BSPC, KC_LCTL, MO(_DR), DR_SPC,  ALT_ESC, KC_MINS, KC_SLSH, KC_ENT ),
 
   [_DL] = LAYOUT( /* [> LOWER <] */
     KC_ESC,  KC_PSCR, KC_PAUS, KC_F1, KC_F2,                    KC_F3,  KC_F4,  KC_INS,  KC_DELT, KC_BSPC ,
