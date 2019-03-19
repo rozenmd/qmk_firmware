3408971cb5bd7fecd6c38206420975579af2949f
Ibnu D. Aji <iaji@siskam.link>
2019-03-19 19:22:47 +0700

    modified layout a bit.

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/daktil/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/daktil/keymap.c
@@ -31,15 +31,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_A,    KC_O,    KC_E,    KC_U, KC_I,                                     KC_D, KC_H, KC_T,    KC_N,   KC_S,
   KC_QUOT, KC_Q,    KC_J,    KC_K, KC_X,                                     KC_B, KC_M, KC_W,    KC_V,   KC_Z,
            KC_TAB,  KC_LGUI,                                                             KC_MINS, KC_SLSH,
-                                         KC_BSPC, SFT_ESC,  RS_ENT,  KC_SPC,
+                                         KC_BSPC, SFT_ESC,  RAISE,   KC_SPC,
                                          KC_LCTL, LW_DEL,   ALT_ENT, KC_LCTL,
                                          KC_LALT, KC_ESC,   KC_LSFT, KC_LALT
 ),
 
 [_RAISE] = LAYOUT(
     KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                             KC_BSLS, KC_7, KC_8,   KC_9, KC_ASTR ,
-    KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                              KC_EQL,  KC_4, KC_5,   KC_6, KC_PLUS ,
-    KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                             KC_GRV,  KC_1, KC_2,   KC_3, KC_PGUP ,
+    KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                              KC_EQL,  KC_4, KC_5,   KC_6, KC_ENT ,
+    KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                             KC_GRV,  KC_1, KC_2,   KC_3, KC_PLUS ,
              KC_INS,  KC_LGUI,                                                        KC_0, KC_DOT,
                                                KC_DELT, ____,    ____, ____,
                                                ____,    ____,    ____, ____,
