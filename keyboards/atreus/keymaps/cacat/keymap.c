409d116f1b2fb815ead2b3ee6a74052b87ebb88f
Ibnu D. Aji <iaji@siskam.link>
2019-05-12 23:26:05 +0700

    mebuh

--- keyboards/atreus/keymaps/cacat/keymap.c
+++ keyboards/atreus/keymaps/cacat/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,   KC_G,    KC_C,    KC_R,    KC_L   ,
     KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,   KC_H,    KC_T,    KC_N,    KC_S   ,
     KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,                      KC_B,   KC_M,    KC_W,    KC_V,    KC_Z   ,
-    KC_ESC,  KC_TAB,  KC_LGUI, SH_ESC,  DL_BSPC, KC_LCTL, MO(_DR), DR_SPC, ALT_ENT, KC_MINS, KC_SLSH, CT_ENT ),
+    CT_ESC,  KC_TAB,  KC_LGUI, SH_ESC,  DL_BSPC, KC_LCTL, MO(_DR), DR_SPC, ALT_ENT, KC_MINS, KC_SLSH, CT_ENT ),
 
   [_DL] = LAYOUT( /* [> LOWER <] */
     KC_ESC,  KC_PSCR, KC_PAUS, KC_F1, KC_F2,                    KC_F3,  KC_F4,  KC_INS,  KC_DELT, KC_BSPC ,
