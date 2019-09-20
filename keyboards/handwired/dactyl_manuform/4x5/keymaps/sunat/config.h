7b0c07eefb4fcb04e48dd9c67b324071a2a7c0af
Ibnu D. Aji <iaji@siskam.link>
2019-09-21 01:05:45 +0700

    idk anymore what should be done.

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/sunat/config.h
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/sunat/config.h
@@ -22,6 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "../../config.h"
 
+#define TAPPING_TERM 500
+
 /* Use I2C or Serial, not both */
 
 #define USE_SERIAL
