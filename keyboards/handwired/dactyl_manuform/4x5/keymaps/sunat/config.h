4495d1b5171dffefab94f9c5094ca379dd5355c0
Ibnu D. Aji <iaji@siskam.link>
2019-09-21 23:35:26 +0700

    nkro enabled.

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/sunat/config.h
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/sunat/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "../../config.h"
 
-#define TAPPING_TERM 500
+#define FORCE_NKRO
 
 /* Use I2C or Serial, not both */
 
