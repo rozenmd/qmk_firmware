a9242510298c88e589fc1afc118826a9975e1430
Ibnu D. Aji <iaji@siskam.link>
2019-10-01 20:50:29 +0700

    added new keymaps

---
+++ keyboards/atreus/keymaps/atrpro/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "config_common.h"
+
+#undef MATRIX_ROW_PINS
+#undef MATRIX_COL_PINS
+#undef UNUSED_PINS
+
+// Pin configuration for github.com/ibnuda/atreus/tree/promicro
+#define MATRIX_ROW_PINS { F4, B2, B4, B5 }
+#define MATRIX_COL_PINS { F5, F6, F7, B1, B3, B6, E6, D7, C6, D4, D0 }
+#define UNUSED_PINS
