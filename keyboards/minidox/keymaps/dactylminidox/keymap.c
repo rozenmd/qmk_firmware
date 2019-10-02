66f3891457f8847f914d5123382f20174482eaef
Ibnu D. Aji <iaji@siskam.link>
2019-10-02 20:49:37 +0700

    keymap iteration.

--- keyboards/minidox/keymaps/dactylminidox/keymap.c
+++ keyboards/minidox/keymaps/dactylminidox/keymap.c
@@ -22,30 +22,19 @@ enum custom_keycodes {
 #define CALTDEL LCTL(LALT(KC_DEL))
 #define TSKMGR LCTL(LSFT(KC_ESC))
 
-#define LW_BSPC LT(_LOWER, KC_BSPC)
-#define SH_ESC  SFT_T(KC_ESC)
-#define CT_TAB  CTL_T(KC_TAB)
+#define AD_DEL  LT(_ADJUST, KC_DELT)
+#define AD_SPC  LT(_ADJUST, KC_SPC)
 #define AL_ENT  ALT_T(KC_ENT)
 #define CT_SLSH CTL_T(KC_SLSH)
+#define CT_TAB  CTL_T(KC_TAB)
+#define LW_BSPC LT(_LOWER, KC_BSPC)
 #define RS_SPC  LT(_RAISE, KC_SPC)
+#define SH_ESC  SFT_T(KC_ESC)
+
+#define ADJUST  MO(_ADJUST)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Qwerty
- *
- * ,----------------------------------.           ,----------------------------------.
- * |   Q  |   W  |   E  |   R  |   T  |           |   Y  |   U  |   I  |   O  |   P  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |   A  |   S  |   D  |   F  |   G  |           |   H  |   J  |   K  |   L  |   ;  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  | Ctrl | LOWER|      |    |      | RAISE| Shift|
- *                  `-------------| Space|    |BckSpc|------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
 [_QWERTY] = LAYOUT( \
   KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,         KC_F,    KC_G,    KC_C,    KC_R,  KC_L, \
   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,         KC_D,    KC_H,    KC_T,    KC_N,  KC_S, \
@@ -53,64 +42,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     LW_BSPC, SH_ESC,  CT_TAB,       CT_SLSH, AL_ENT,  RS_SPC                \
 ),
 
-/* Raise
- *
- * ,----------------------------------.           ,----------------------------------.
- * |   1  |   2  |   3  |   4  |   5  |           |   6  |   7  |   8  |   9  |   0  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Tab | Left | Down |  Up  | Right|           |      |   -  |   =  |   [  |   ]  |
- * |------+------+------+------+------|           |------+------+------+------+------|
-
- *                  ,--------------------.    ,------,-------------.
- *                  |      | LOWER|      |    |      | RAISE|      |
- *                  `-------------|      |    |      |------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
 [_RAISE] = LAYOUT( \
     KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_BSLS, KC_7,    KC_8,   KC_9,   KC_ASTR ,
     KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                    KC_EQL,  KC_4,    KC_5,   KC_6,   KC_ENT ,
     KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                   KC_GRV,  KC_1,    KC_2,   KC_3,   KC_PLUS ,
-                      _______, _______, _______,                   _______, KC_0,    KC_DOT                    \
+                      ADJUST,  _______, KC_0,                      _______, _______, KC_DOT                    \
 ),
 
-/* Lower
- *
- * ,----------------------------------.           ,----------------------------------.
- * |   !  |   @  |   #  |   $  |   %  |           |   ^  |   &  |   *  |   (  |   )  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Esc |      |      |      |      |           |      |   _  |   +  |   {  |   }  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Caps|   ~  |      |      |      |           |      |      |      |   |  |   "  |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |      | LOWER|      |    |      | RAISE|  Del |
- *                  `-------------|      |    | Enter|------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
 [_LOWER] = LAYOUT( \
-    KC_ESC,  KC_PSCR, KC_PAUS, KC_F1, KC_F2,                    KC_F3,  KC_F4,   KC_INS,  KC_DELT, KC_BSPC ,
+    KC_ESC,  KC_PSCR, KC_PAUS, KC_F1, KC_F2,                    KC_F3,  KC_F4,   KC_INS,  KC_MINS, KC_BSPC ,
     KC_CLCK, KC_TAB,  KC_PGUP, KC_F5, KC_F6,                    KC_F7,  KC_F8,   KC_HOME, KC_LALT, KC_ENT  ,
-    KC_LSFT, KC_SLCK, KC_PGDN, KC_F9, KC_F10,                   KC_F11, KC_F12,  KC_END,  KC_MINS, KC_SLSH ,
-                    _______, _______, _______,                  KC_ENT, _______, KC_DEL                    \
+    KC_LSFT, KC_LSFT, KC_PGDN, KC_F9, KC_F10,                   KC_F11, KC_F12,  KC_END,  KC_MINS, KC_SLSH ,
+                      _______, _______, _______,                KC_ENT, KC_DEL,  ADJUST                    \
 ),
 
-/* Adjust (Lower + Raise)
- *
- * ,----------------------------------.           ,----------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |           |   F6 |  F7  |  F8  |  F9  |  F10 |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  F11 |  F12 |      |      |      |           |      |      |      |Taskmg|caltde|
- * |------+------+------+------+------|           |------+------+------+------+------|
- * | Reset|      |      |      |      |           |      |      |      |      |      |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |      | LOWER|      |    |      | RAISE|      |
- *                  `-------------|      |    |      |------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
 [_ADJUST] =  LAYOUT( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, \
   KC_F11,  KC_F12,  _______, _______, _______,      _______, _______, _______, TSKMGR, CALTDEL, \
@@ -126,43 +71,43 @@ void persistant_default_layer_set(uint16_t default_layer) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(tone_qwerty);
-        #endif
-        persistant_default_layer_set(1UL<<_QWERTY);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
-      }
-      return false;
-      break;
+	case QWERTY:
+	  if (record->event.pressed) {
+		#ifdef AUDIO_ENABLE
+		  PLAY_SONG(tone_qwerty);
+		#endif
+		persistant_default_layer_set(1UL<<_QWERTY);
+	  }
+	  return false;
+	  break;
+	case LOWER:
+	  if (record->event.pressed) {
+		layer_on(_LOWER);
+		update_tri_layer(_LOWER, _RAISE, _ADJUST);
+	  } else {
+		layer_off(_LOWER);
+		update_tri_layer(_LOWER, _RAISE, _ADJUST);
+	  }
+	  return false;
+	  break;
+	case RAISE:
+	  if (record->event.pressed) {
+		layer_on(_RAISE);
+		update_tri_layer(_LOWER, _RAISE, _ADJUST);
+	  } else {
+		layer_off(_RAISE);
+		update_tri_layer(_LOWER, _RAISE, _ADJUST);
+	  }
+	  return false;
+	  break;
+	case ADJUST:
+	  if (record->event.pressed) {
+		layer_on(_ADJUST);
+	  } else {
+		layer_off(_ADJUST);
+	  }
+	  return false;
+	  break;
   }
   return true;
 }
