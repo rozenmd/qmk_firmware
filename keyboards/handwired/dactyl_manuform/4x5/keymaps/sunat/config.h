0da72e7fc1ca0cb7a43a886473a656bae779fec2
Ibnu D. Aji <iaji@siskam.link>
2019-10-03 21:31:48 +0700

    keymap iteration.

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/sunat/config.h
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/sunat/config.h
@@ -33,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #undef TAPPING_TERM
 >>>>>>> keymap iteration.
 #define TAPPING_TERM 200
-#define IGNORE_MOD_TAP_INTERUPT
+#define IGNORE_MOD_TAP_INTERRUPT
 
 =======
 >>>>>>> added sunat
