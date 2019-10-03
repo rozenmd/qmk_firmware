b5415061f36c1bfaca58831ef6e39e59a30a1163
Ibnu D. Aji <iaji@siskam.link>
2019-10-03 22:10:01 +0700

    keymap iteration.

--- keyboards/minidox/keymaps/dactylminidox/keymap.c
+++ keyboards/minidox/keymaps/dactylminidox/keymap.c
@@ -18,9 +18,6 @@ enum custom_keycodes {
   ADJUST,
 };
 
-// Defines for task manager and such
-#define CALTDEL LCTL(LALT(KC_DEL))
-#define TSKMGR LCTL(LSFT(KC_ESC))
 
 #define AD_DEL  LT(_ADJUST, KC_DELT)
 #define AD_SPC  LT(_ADJUST, KC_SPC)
@@ -31,13 +28,26 @@ enum custom_keycodes {
 #define RS_SPC  LT(_RAISE, KC_SPC)
 #define SH_ESC  SFT_T(KC_ESC)
 
-#define ADJUST  MO(_ADJUST)
+
+#define AL_E    ALT_T(KC_E)
+#define AL_T    ALT_T(KC_T)
+#define CT_N    CTL_T(KC_N)
+#define CT_O    CTL_T(KC_O)
+#define GU_H    LGUI_T(KC_H)
+#define GU_U    LGUI_T(KC_U)
+#define SH_A    SFT_T(KC_A)
+#define SH_S    SFT_T(KC_S)
+
+#define CALTDEL  LCTL(LALT(KC_DEL))
+#define EXPLORER LGUI(KC_E)
+#define LOCKGUI  LGUI(KC_L)
+#define TASKMGR  LCTL(LSFT(KC_ESC))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT( \
   KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,         KC_F,    KC_G,    KC_C,    KC_R,  KC_L, \
-  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,         KC_D,    KC_H,    KC_T,    KC_N,  KC_S, \
+  SH_A,    CT_O,    AL_E,    GU_U,    KC_I,         KC_D,    GU_H,    AL_T,    CT_N,  SH_S, \
   KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,         KC_B,    KC_M,    KC_W,    KC_V,  KC_Z, \
                     LW_BSPC, SH_ESC,  CT_TAB,       CT_SLSH, AL_ENT,  RS_SPC                \
 ),
@@ -57,10 +67,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_ADJUST] =  LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, \
-  KC_F11,  KC_F12,  _______, _______, _______,      _______, _______, _______, TSKMGR, CALTDEL, \
-  RESET,   _______, _______, _______, RESET,        _______, _______, _______, _______,  _______, \
-                    _______, _______, _______,      _______,  _______, _______                    \
+  _______, EXPLORER, KC_UP,   _______, _______,      _______, _______, _______, _______, LOCKGUI, \
+  TASKMGR, KC_LEFT,  KC_DOWN, KC_RGHT, _______,      _______, _______, KC_LGUI, _______, CALTDEL, \
+  _______, _______,  _______, _______, RESET,        _______, _______, _______, _______, _______, \
+                     _______, _______, _______,      _______,  _______, _______                    \
 )
 };
 
@@ -71,43 +81,43 @@ void persistant_default_layer_set(uint16_t default_layer) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-	case QWERTY:
-	  if (record->event.pressed) {
-		#ifdef AUDIO_ENABLE
-		  PLAY_SONG(tone_qwerty);
-		#endif
-		persistant_default_layer_set(1UL<<_QWERTY);
-	  }
-	  return false;
-	  break;
-	case LOWER:
-	  if (record->event.pressed) {
-		layer_on(_LOWER);
-		update_tri_layer(_LOWER, _RAISE, _ADJUST);
-	  } else {
-		layer_off(_LOWER);
-		update_tri_layer(_LOWER, _RAISE, _ADJUST);
-	  }
-	  return false;
-	  break;
-	case RAISE:
-	  if (record->event.pressed) {
-		layer_on(_RAISE);
-		update_tri_layer(_LOWER, _RAISE, _ADJUST);
-	  } else {
-		layer_off(_RAISE);
-		update_tri_layer(_LOWER, _RAISE, _ADJUST);
-	  }
-	  return false;
-	  break;
-	case ADJUST:
-	  if (record->event.pressed) {
-		layer_on(_ADJUST);
-	  } else {
-		layer_off(_ADJUST);
-	  }
-	  return false;
-	  break;
+    case QWERTY:
+      if (record->event.pressed) {
+#ifdef AUDIO_ENABLE
+        PLAY_SONG(tone_qwerty);
+#endif
+        persistant_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case ADJUST:
+      if (record->event.pressed) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
+      }
+      return false;
+      break;
   }
   return true;
 }
