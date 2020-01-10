601930cdeca0da094416cbc633cb81d817feebf4
Ibnu D. Aji <iaji@siskam.link>
2020-01-10 18:27:43 +0700

    added reset button.

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/default/keymap.c
@@ -1,9 +1,10 @@
 #include QMK_KEYBOARD_H
 
 
-#define _BASE 0
-#define _RAISE 1
-#define _LOWER 2
+#define _BASE   0
+#define _RAISE  1
+#define _LOWER  2
+#define _ADJUST 3
 
 // Fillers to make layering more clear
 
@@ -23,6 +24,7 @@
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
+#define ADJUS MO(_ADJUST)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -85,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ____,  ____,                                                          KC_MB1,  KC_MB2,              \
                                    ____, ____,  ____, ____,                                                 \
                                    ____, ____,  ____, ____,                                                 \
-                                   ____, ____,  ____, ____                                                  \
+                                   ____, ____,  ____, ADJUS                                                 \
 ),
 /* Lower
  * ,----------------------------------,                             ,----------------------------------,
@@ -109,14 +111,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_LOWER] = LAYOUT( \
-  KC_F1,   KC_F2,  KC_F3,   KC_F4,  KC_F5,                            KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  \
-  KC_1,    KC_2,   KC_3,    KC_4,   KC_5,                             KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    \
-  KC_EXLM, KC_AT,  KC_HASH, KC_DLR, KC_PERC,                          KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, \
-           KC_F11, KC_F12,                                                               ____,    ____,             \
-                                             ____, ____,  ____, ____,                                               \
-                                             ____, ____,  ____, ____,                                               \
-                                             ____, ____,  ____, ____                                                \
-)
+  KC_F1,   KC_F2,  KC_F3,   KC_F4,  KC_F5,                             KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  \
+  KC_1,    KC_2,   KC_3,    KC_4,   KC_5,                              KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    \
+  KC_EXLM, KC_AT,  KC_HASH, KC_DLR, KC_PERC,                           KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, \
+           KC_F11, KC_F12,                                                                ____,    ____,             \
+                                             ____,  ____,  ____, ____,                                               \
+                                             ____,  ____,  ____, ____,                                               \
+                                             ADJUS, ____,  ____, ____                                                \
+),
+[_ADJUST] = LAYOUT( \
+  ____, ____,  ____, ____,  ____,                          ____, ____, ____, ____, ____, \
+  ____, ____,  ____, ____,  ____,                          ____, ____, ____, ____, ____, \
+  ____, ____,  ____, ____,  RESET,                         ____, ____, ____, ____, ____, \
+        ____,  ____,                                                   ____, ____,       \
+                                 ____, ____,  ____, ____,                                \
+                                 ____, ____,  ____, ____,                                \
+                                 ____, ____,  ____, ____                                 \
+),
 };
 
 void persistent_default_layer_set(uint16_t default_layer) {
