fd836b12607e0afd691b86fce04c9f23b414a48e
Ibnu D. Aji <iaji@siskam.link>
2020-02-18 18:51:39 +0700

    whatever.

--- keyboards/atreus/keymaps/ibnuda/keymap.c
+++ keyboards/atreus/keymaps/ibnuda/keymap.c
@@ -233,9 +233,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT(
     KC_EXLM,KC_AT,  KC_UP,  KC_LCBR,KC_RCBR,                    KC_BSLS,KC_7,   KC_8,   KC_9,   KC_ASTR ,
-    KC_HASH,KC_LEFT,KC_DOWN,KC_RGHT,KC_DLR,                     KC_EQL, KC_4,   KC_5,   KC_6,   KC_TILD ,
+    KC_HASH,KC_LEFT,KC_DOWN,KC_RGHT,KC_DLR,                     KC_EQL, KC_4,   KC_5,   KC_6,   KC_0 ,
     KC_LBRC,KC_RBRC,KC_LPRN,KC_RPRN,KC_AMPR,                    KC_GRV, KC_1,   KC_2,   KC_3,   KC_PLUS ,
-    _______,_______,_______,_______,ADDDD,  _______,    ALT_ENT,RS_SPC, _______,KC_0,   _______,_______
+    _______,_______,_______,_______,ADDDD,  _______,    ALT_ENT,RS_SPC, _______,_______,_______,_______
 ),
 [_LOWER] = LAYOUT(
     KC_ESC, KC_QUES,KC_UNDS,KC_F1,  KC_F2,                      KC_F3,  KC_F4,  KC_MINS,KC_SLSH,KC_BSPC ,
