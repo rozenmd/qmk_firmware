6c51a4b92873ef4276de8b7b05b42208f6cee675
Ibnu D. Aji <iaji@siskam.link>
2020-05-16 12:04:11 +0700

    added enter.

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/default/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                       KC_N, KC_M,    KC_COMM, KC_DOT, KC_QUOT, \
               KC_LBRC, KC_RBRC,                                                   KC_MINS, KC_EQL,                   \
                                       LW_BSPC, CTL_BSPC,  ALT_SPC, RS_SPC,                                           \
-                                      KC_TAB,  KC_HOME,   KC_END,  KC_DEL,                                           \
+                                      KC_TAB,  KC_ENT,   KC_END,  KC_DEL,                                           \
                                       RAISE,   KC_GRV,    KC_LGUI, LOWER
 ),
 
