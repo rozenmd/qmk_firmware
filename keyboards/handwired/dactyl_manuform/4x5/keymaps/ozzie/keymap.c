0320ed380be8a636f5edd7c6fcbfa7b78adcb233
Ibnu D. Aji <iaji@siskam.link>
2020-08-03 17:25:50 +0700

    quote shouldve been slash.

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/ozzie/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/ozzie/keymap.c
@@ -139,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT( \
   KC_Q, KC_W, KC_E,    KC_R,    KC_T,                                       KC_Y, KC_U,    KC_I,    KC_O,   KC_P,    \
   SH_A, CT_S, AL_D,    KC_F,    GU_G,                                       GU_H, KC_J,    AL_K,    CT_L,   SH_SCLN, \
-  KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                       KC_N, KC_M,    KC_COMM, KC_DOT, KC_QUOT, \
+  KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                       KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH, \
               _______, _______,                                                   _______, _______,                  \
                                       LW_BSPC, SFT_ESC,  ALT_ENT, RS_SPC,                                            \
                                       _______, _______,  _______, _______,                                           \
