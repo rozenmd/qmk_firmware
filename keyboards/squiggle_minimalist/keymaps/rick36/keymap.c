4ad8799f93953bdb5e55ef9c4003b6fbea83a51f
Ibnu D. Aji <iaji@siskam.link>
2020-11-20 19:36:07 +0700

    what.

--- keyboards/squiggle_minimalist/keymaps/rick36/keymap.c
+++ keyboards/squiggle_minimalist/keymaps/rick36/keymap.c
@@ -166,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_SCLN,KC_COMM,KC_DOT, KC_P,   KC_Y,       KC_F,   KC_G,   KC_C,   KC_R,   KC_L,
     SH_A,   CT_O,   AL_E,   MV_U,   GU_I,       GU_D,   KC_H,   AL_T,   CT_N,   SH_S,
     KC_QUOT,KC_Q,   KC_J,   KC_K,   KC_X,       KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,
-                    LOWER,  LW_BSPC,SFT_ESC,    ALT_ENT,RS_SPC ,RAISE
+                    KC_LGUI,LW_BSPC,SFT_ESC,    ALT_ENT,RS_SPC, RAISE
 ),
 
 [_RAISE] = LAYOUT_split_3x5_3(
