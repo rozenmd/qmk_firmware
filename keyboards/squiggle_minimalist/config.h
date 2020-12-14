b63c9c5905dd5e3a6051112a2770ed1b13a55cf1
Ibnu D. Aji <iaji@siskam.link>
2020-12-14 17:40:47 +0700

    corrected vid and pid.

--- keyboards/squiggle_minimalist/config.h
+++ keyboards/squiggle_minimalist/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
+#define VENDOR_ID    0x1209
+#define PRODUCT_ID   0x6969
 #define DEVICE_VER   0x0001
 #define MANUFACTURER Ibnu D. Aji
 #define PRODUCT      squiggle_minimalist
