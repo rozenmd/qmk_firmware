431c90852a989ca6cf01c26a41aa015150f61338
Ibnu D. Aji <iaji@siskam.link>
2021-01-20 19:00:25 +0700

    added reset.

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/default/keymap.c
@@ -13,6 +13,8 @@
 #define CTL_BSPC CTL_T(KC_BSPC)
 #define ALT_SPC  ALT_T(KC_SPC)
 #define SFT_ENT  SFT_T(KC_ENT)
+#define RS_SPC   LT(_RAISE, KC_SPC)
+#define LW_BSPC  LT(_RAISE, KC_BSPC)
 
 #define KC_ML KC_MS_LEFT
 #define KC_MR KC_MS_RIGHT
@@ -52,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_A, KC_S, KC_D,    KC_F,    KC_G,                                       KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN, \
   KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                       KC_N, KC_M,    KC_COMM, KC_DOT, KC_QUOT, \
               KC_LBRC, KC_RBRC,                                                   KC_MINS, KC_EQL,                   \
-                                      SFT_ESC, CTL_BSPC,  ALT_SPC, SFT_ENT,                                          \
+                                      SFT_ESC, LW_BSPC,   RS_SPC,  SFT_ENT,                                          \
                                       KC_TAB,  KC_HOME,   KC_END,  KC_DEL,                                           \
                                       RAISE,   KC_GRV,    KC_LGUI, LOWER
 ),
