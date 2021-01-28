dcf356b0eefd2eede87c505d4b178c7f803843a2
Max Rozen <rozenmd@gmail.com>
2021-01-28 12:56:40 +1100

    Update keymap.c

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G,       KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,
         KC_LCTL,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B,       KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
                         KC_LBRC,KC_RBRC,                                    KC_PLUS,KC_EQL,
-                                        LW_BSPC,LW_BSPC,    KC_ENT, RS_SPC,
+                                        KC_LALT,LW_BSPC,    KC_ENT, RS_SPC,
                                         KC_TAB, KC_HOME,    KC_END, KC_DEL,
                                         KC_BSPC,KC_ESC,     KC_LGUI,KC_LALT
     ),
